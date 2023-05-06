6477fe8673a9a8cb5e295a0f33b1f3a76916e981
victan78 <116087124+Victan78@users.noreply.github.com>
2023-05-07 01:24:24 +0200

    g h

--- routes/api.php
+++ routes/api.php
@@ -16,9 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 Route::post('/utilisateur/inscription', [UserController::class, 'inscription']);
 Route::post('/utilisateur/connexion', [UserController::class, 'connexion']);
-Route::group(['middleware' => ['auth:sanctum']], function () {
-    Route::post('/utilisateur/compte/deconnexion', [UserController::class, 'deconnexion']);
-    Route::post('/utilisateur/compte/suppression', [UserController::class, 'suppression']);
- 
-}
-);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
